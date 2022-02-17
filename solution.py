da62ae013191ca664bbbc01a051c7a5ccfaa13ab
tianqizhang21 <tq.zhang@hotmail.com>
2022-02-17 13:46:17 -0500

    Try 4

--- solution.py
+++ solution.py
@@ -12,7 +12,7 @@ def welcome_assignment_answers(question):
     elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer = "mtls"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "No"
+        answer = "Yes"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
     elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
